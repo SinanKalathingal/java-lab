27526bfe9e13ad10e1b3eb7ab722f116deef7f8f
Mohmdsinan <sinankdty@gmail.com>
2026-09-16 22:29:10 +0530

    removed all files

--- MyClass.java
+++
@@ -1,8 +0,0 @@
-class MyClass {
-	int i;
-	public static void main(String a[]){
-		MyClass MyObj = new MyClass();
-		MyObj.i = 2;
-		System.out.println("First Java Programme "+ MyObj.i);
-	}
-}
--- inheritance demo.java
+++
@@ -1,3 +0,0 @@
-class inheritance demo {
-public static void main(String args[]){
-A superOb=new()
